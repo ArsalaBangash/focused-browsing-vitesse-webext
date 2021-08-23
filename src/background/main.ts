58256a5bfe80919934572a30496ecaaaac88f360
Arsala Grey <arsala@grey.software>
2021-08-23 22:34:37 +0500

    Initial commit

---
+++ src/background/main.ts
@@ -0,0 +1,50 @@
+import { sendMessage, onMessage } from 'webext-bridge'
+import browser, { Tabs } from 'webextension-polyfill'
+
+if (import.meta.hot)
+  // @ts-expect-error for background HMR on dev
+  import('/@vite/client')
+
+browser.runtime.onInstalled.addListener((): void => {
+  // eslint-disable-next-line no-console
+  console.log('Extension installed')
+})
+
+let previousTabId = 0
+
+// communication example: send previous tab title from background page
+// see shim.d.ts for type declaration
+browser.tabs.onActivated.addListener(async({ tabId }) => {
+  if (!previousTabId) {
+    previousTabId = tabId
+    return
+  }
+
+  let tab: Tabs.Tab
+
+  try {
+    tab = await browser.tabs.get(previousTabId)
+    previousTabId = tabId
+  }
+  catch {
+    return
+  }
+
+  // eslint-disable-next-line no-console
+  console.log('previous tab', tab)
+  sendMessage('tab-prev', { title: tab.title }, { context: 'content-script', tabId })
+})
+
+onMessage('get-current-tab', async() => {
+  try {
+    const tab = await browser.tabs.get(previousTabId)
+    return {
+      title: tab?.id,
+    }
+  }
+  catch {
+    return {
+      title: undefined,
+    }
+  }
+})
---
+++ src/manifest.ts
@@ -0,0 +1,51 @@
+import fs from 'fs-extra'
+import type { Manifest } from 'webextension-polyfill'
+import type PkgType from '../package.json'
+import { isDev, port, r } from '../scripts/utils'
+
+export async function getManifest(): Promise<Manifest.WebExtensionManifest> {
+  const pkg: typeof PkgType = await fs.readJSON(r('package.json'))
+  // update this file to update this manifest.json
+  // can also be conditional based on your need
+  return {
+    manifest_version: 2,
+    name: pkg.displayName || pkg.name,
+    version: pkg.version,
+    description: pkg.description,
+    browser_action: {
+      default_icon: './assets/icon-512.png',
+      default_popup: './dist/popup/index.html',
+    },
+    options_ui: {
+      page: './dist/options/index.html',
+      open_in_tab: true,
+      chrome_style: false,
+    },
+    background: {
+      page: './dist/background/index.html',
+      persistent: false,
+    },
+    content_scripts: [
+      {
+        matches: ['http://*/*', 'https://*/*'],
+        js: ['./dist/contentScripts/index.global.js'],
+      },
+    ],
+    icons: {
+      16: './assets/icon-512.png',
+      48: './assets/icon-512.png',
+      128: './assets/icon-512.png',
+    },
+    permissions: [
+      'tabs',
+      'storage',
+      'activeTab',
+      'http://*/',
+      'https://*/',
+    ],
+    // this is required on dev for Vite script to load
+    content_security_policy: isDev
+      ? `script-src \'self\' http://localhost:${port}; object-src \'self\'`
+      : undefined,
+  }
+}
